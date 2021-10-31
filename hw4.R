9b9c74e2c034d4f19a5687ddc621463c5a9ce482
herryhen <57007288+herryhen@users.noreply.github.com>
2021-10-31 10:52:19 -0400

    Add files via upload

--- hw4.R
+++ hw4.R
@@ -1,14 +1,40 @@
+install.packages("rnrfa")
+
+
+library(ggmap)
 library(tidyverse)
-dd <- read_excel(".../hw4.xlsx")
+library(sf)
+library(googleway)
+library(ggspatial)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(geogrid)
+library(viridis)
+library(gganimate)
+library(tmap)
+library(spData) #dataset
+library(spDataLarge) #dataset
+library(tmap)
+library(mapview)
+library(rnrfa)
 
-temp <- dd[[2]] #https://www.reddit.com/r/rstats/comments/9bnvox/why_cant_i_plot_using_dataframei/
-year <- dd[[1]]
+#map_lnd = tm_shape(lnd) + tm_borders(col="black") + tm_fill(col="white")
+# tiles = "http://a.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+# map_lnd + tm_basemap(server = tiles)
+#mapview(lnd, alpha.regions = 0.15, layer.name="Borough", label="NAME")+
+#  mapview(nz, layer.name="New Zealand", label="Name") +
+#  mapview(hawaii, layer.name="Hawaii", label="NAME") +
+#  mapview(us_states)
 
-ddfit = lm(temp ~ year, dd)
-summary(ddfit)
+lsp <- function (package, all.names = FALSE, pattern) {
+  package <- deparse(substitute(package))
+  ls(pos = paste("package", package, sep = ":"), all.names = all.names, 
+     pattern = pattern)
+}
+lsp(rnrfa)
 
-ggplot(dd, aes(x = year, y = temp)) + 
-  geom_point() +
-  xlab("Year")+ 
-  ylab("Annual temperature anomaly") +
-  stat_smooth(method = "lm", col = "red")
+tmap_mode("view") #interactive map
+mapview(world, alpha.regions = 0.15, layer.name="Countries", label="name") +
+  tm_basemap("OpenStreetMap")
+
+#I was trying to create an interactive map that showed the world countries but I ran out of time to do a stronger concept.
