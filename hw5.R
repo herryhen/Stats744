61b9a5d0be43326b1c4d1ffd66ed22a0383ef023
Ben Bolker <bbolker@gmail.com>
2021-11-01 19:56:41 -0400

    hw5 comments

--- hw5.R
+++ hw5.R
@@ -1,6 +1,10 @@
-install.packages("rnrfa")
+## BMB: please do **not** use install.packages() in a script
+## this decision should be left to user
+## install.packages("rnrfa")
 
-
+## this is a lot of packages. Do you really need them all?
+## Maybe comment what some of the
+## less familiar ones are for??
 library(ggmap)
 library(tidyverse)
 library(sf)
@@ -14,21 +18,28 @@ library(gganimate)
 library(tmap)
 library(spData) #dataset
 library(spDataLarge) #dataset
-library(tmap)
+library(tmap)  ## BMB: duplicated line
 library(mapview)
 library(rnrfa)
 
-#map_lnd = tm_shape(lnd) + tm_borders(col="black") + tm_fill(col="white")
-# tiles = "http://a.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-# map_lnd + tm_basemap(server = tiles)
-#mapview(lnd, alpha.regions = 0.15, layer.name="Borough", label="NAME")+
-#  mapview(nz, layer.name="New Zealand", label="Name") +
-#  mapview(hawaii, layer.name="Hawaii", label="NAME") +
-#  mapview(us_states)
+## I uncommented these so your code would do something ...
+map_lnd = tm_shape(lnd) + tm_borders(col="black") + tm_fill(col="white")
+tiles = "http://a.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+map_lnd + tm_basemap(server = tiles)
+mapview(lnd, alpha.regions = 0.15, layer.name="Borough", label="NAME")+
+  mapview(nz, layer.name="New Zealand", label="Name") +
+  mapview(hawaii, layer.name="Hawaii", label="NAME") +
+  mapview(us_states)
 
+## BMB: are NAME and Name the same? why does capitalization differ?
+
+## BMB: where is this from?
+## looks similar to this? https://rdrr.io/github/GarrettMooney/moonmisc/src/R/lsp.R
+## it's OK to use code from the web but you **MUST** cite your sources.
+## otherwise it's plagiarism.
 lsp <- function (package, all.names = FALSE, pattern) {
   package <- deparse(substitute(package))
-  ls(pos = paste("package", package, sep = ":"), all.names = all.names, 
+  ls(pos = paste("package", package, sep = ":"), all.names = all.names,
      pattern = pattern)
 }
 lsp(rnrfa)
@@ -36,5 +47,15 @@ lsp(rnrfa)
 tmap_mode("view") #interactive map
 mapview(world, alpha.regions = 0.15, layer.name="Countries", label="name") +
   tm_basemap("OpenStreetMap")
-
 #I was trying to create an interactive map that showed the world countries but I ran out of time to do a stronger concept.
+
+## BMB: Error in (function (classes, fdef, mtable)  :
+##   unable to find an inherited method for function ‘mapView’ for signature ‘"tmap"’
+## does this actually work for you?
+
+## there's almost nothing here (there was *nothing* here that worked at all,
+## until I uncommented some stuff.  You didn't explain anything.
+## Everything *might* be copied from somewhere, but I can't tell.
+
+## mark: 1.2/3
+
